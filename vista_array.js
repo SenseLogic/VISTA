365ed15de7bc7b67aa2f1ae68e0c917a5ed10d5f
senselogic <ecstatic.coder@gmail.com>
2020-02-01 08:24:19 +0100

    % carousel, % js

--- vista_array.js
+++ vista_array.js
@@ -2,51 +2,6 @@
 
 // -- FUNCTIONS
 
-function GetDump(
-    value
-    )
-{
-    if ( value === undefined )
-    {
-        return "undefined";
-    }
-    else if ( value === null )
-    {
-        return "null";
-    }
-    else if ( typeof value === "boolean"
-              || typeof value === "number" )
-    {
-        return "" + value;
-    }
-    else
-    {
-        return JSON.stringify( value );
-    }
-}
-
-// ~~
-
-function Dump(
-    value
-    )
-{
-    console.log( GetDump( value ) );
-}
-
-// ~~
-
-Array.prototype.Call = function(
-    node_function
-    )
-{
-    this.forEach( node_function );
-
-    return this;
-}
-
-// ~~
-
 Array.prototype.DumpNodes = function(
     )
 {
@@ -57,6 +12,19 @@ Array.prototype.DumpNodes = function(
     {
         console.dir( node );
     }
+
+    return this;
+}
+
+// ~~
+
+Array.prototype.Apply = function(
+    node_function
+    )
+{
+    this.forEach( element_function );
+
+    return this;
 }
 
 // ~~
@@ -310,46 +278,4 @@ function GetNodes(
     )
 {
     return Array.from( document.QuerySelectorAll( node_selector ) );
-}
-
-// ~~
-
-function CallWhenReady(
-    initialization_function
-    )
-{
-    if ( document.readyState === "complete"
-         || document.readyState === "interactive" )
-    {
-        setTimeout( initialization_function, 1 );
-    }
-    else
-    {
-        document.addEventListener( "DOMContentLoaded", initialization_function );
-    }
-}
-
-// ~~
-
-function IsNodeInViewport(
-    node,
-    bottom_offset = 0,
-    top_offset = 0,
-    left_offset = 0,
-    right_offset = 0
-    )
-{
-    var
-        bounding_client_rectangle;
-
-    bounding_client_rectangle = node.getBoundingClientRect();
-
-    return (
-        ( bounding_client_rectangle.height > 0
-          || bounding_client_rectangle.width > 0 )
-        && bounding_client_rectangle.bottom >= bottom_offset
-        && bounding_client_rectangle.right >= right_offset
-        && bounding_client_rectangle.top + top_offset <= ( window.innerHeight || document.documentElement.clientHeight )
-        && bounding_client_rectangle.left + left_offset <= ( window.innerWidth || document.documentElement.clientWidth )
-        );
 }
---
+++ vista_base.js
@@ -0,0 +1,88 @@
+"use strict";
+
+// -- FUNCTIONS
+
+function GetJsonValue(
+    json_text
+    )
+{
+    return JSON.parse( json_text );
+}
+
+// ~~
+
+function GetJsonText(
+    value
+    )
+{
+    return JSON.stringify( value );
+}
+
+// ~~
+
+function Dump(
+    value
+    )
+{
+    console.log( JSON.stringify( value ) );
+}
+
+// ~~
+
+function DumpNode(
+    node
+    )
+{
+    console.dir( node );
+}
+
+// ~~
+
+function Print(
+    value
+    )
+{
+    console.log( value );
+}
+
+// ~~
+
+function Postpone(
+    start_function
+    )
+{
+    if ( document.readyState === "complete"
+         || document.readyState === "interactive" )
+    {
+        setTimeout( start_function, 1 );
+    }
+    else
+    {
+        document.addEventListener( "DOMContentLoaded", start_function );
+    }
+}
+
+// ~~
+
+function IsVisibleNode(
+    node,
+    bottom_offset = 0,
+    top_offset = 0,
+    left_offset = 0,
+    right_offset = 0
+    )
+{
+    var
+        bounding_client_rectangle;
+
+    bounding_client_rectangle = node.getBoundingClientRect();
+
+    return (
+        ( bounding_client_rectangle.height > 0
+          || bounding_client_rectangle.width > 0 )
+        && bounding_client_rectangle.bottom >= bottom_offset
+        && bounding_client_rectangle.right >= right_offset
+        && bounding_client_rectangle.top + top_offset <= ( window.innerHeight || document.documentElement.clientHeight )
+        && bounding_client_rectangle.left + left_offset <= ( window.innerWidth || document.documentElement.clientWidth )
+        );
+}
