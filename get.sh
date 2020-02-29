581d2b0ca023cb1ba940c7b5465695435b6994ae
senselogic <ecstatic.coder@gmail.com>
2020-02-29 10:33:09 +0100

    % carousel, % sample

--- get.sh
+++ get.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-sudo apt install gcc g++ nodejs npm
+sudo apt install curl python-software-properties build-essential gcc g++
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo apt install -y nodejs
 sudo npm install -g stylus
