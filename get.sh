044ddf79f92f5240baff849ca6644ffb6ee60866
senselogic <ecstatic.coder@gmail.com>
2020-05-16 20:27:20 +0200

    % get

--- get.sh
+++ get.sh
@@ -2,5 +2,7 @@
 sudo apt install curl python-software-properties build-essential gcc g++
 curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
 sudo apt install -y nodejs
+sudo npm install -g npm
 sudo npm install -g stylus
 sudo npm install -g http-server
+
