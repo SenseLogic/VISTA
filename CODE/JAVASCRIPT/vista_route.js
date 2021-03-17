8d15d0cb9583c259edea5e4cea08cea72783b929
senselogic <dev.senselogic@gmail.com>
2021-03-17 20:57:27 +0100

    + route

---
+++ CODE/JAVASCRIPT/vista_route.js
@@ -0,0 +1,41 @@
+// -- FUNCTIONS
+
+function GetViewName(
+    route
+    )
+{
+    var
+        separator_character_index;
+
+    separator_character_index = route.indexOf( "#" );
+
+    if ( separator_character_index < 0 )
+    {
+        return route;
+    }
+    else
+    {
+        return substr( route, 0, separator_character_index );
+    }
+}
+
+// ~~
+
+function GetSectionName(
+    route
+    )
+{
+    var
+        separator_character_index;
+
+    separator_character_index = route.indexOf( "#" );
+
+    if ( separator_character_index < 0 )
+    {
+        return "";
+    }
+    else
+    {
+        return route.substring( separator_character_index + 1 );
+    }
+}
