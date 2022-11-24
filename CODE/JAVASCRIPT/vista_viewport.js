8e2fad484d0f76950e8304482b643b6f3bc38f3d
senselogic <dev.senselogic@gmail.com>
2022-11-24 22:07:58 +0100

    % viewport

--- CODE/JAVASCRIPT/vista_viewport.js
+++ CODE/JAVASCRIPT/vista_viewport.js
@@ -4,7 +4,9 @@ function UpdateViewportProperties(
     )
 {
     document.documentElement.style.setProperty( "--viewport-height", window.innerHeight + "px" );
+    document.documentElement.style.setProperty( "--viewport-height-aspect-ratio", window.innerHeight / window.innerWidth );
     document.documentElement.style.setProperty( "--viewport-width", window.innerWidth + "px" );
+    document.documentElement.style.setProperty( "--viewport-width-aspect-ratio", window.innerWidth / window.innerHeight );
 }
 
 // -- STATEMENTS
