a664fff582795e225a557617bab1880e8e7a8780
senselogic <dev.senselogic@gmail.com>
2024-01-25 22:20:08 +0100

    % tracking

--- CODE/JAVASCRIPT/vista_tracking.js
+++ CODE/JAVASCRIPT/vista_tracking.js
@@ -3,9 +3,20 @@
 var
     GoogleAnalyticsTrackingId = "",
     GoogleAnalyticsTrackingIsEnabled = false,
-    GoogleAnalyticsTrackingScript = null;
+    GoogleAnalyticsTrackingScript = null,
+    GoogleTagManagerTrackingId = "",
+    GoogleTagManagerTrackingIsEnabled = false,
+    GoogleTagManagerTrackingScript = null;
 
 // -- FUNCTIONS
+
+function gtag(
+    )
+{
+    window.dataLayer.push( arguments );
+}
+
+// ~~
 
 function TrackRoute(
     )
@@ -21,14 +32,17 @@ function TrackRoute(
             }
             );
     }
-}
 
-// ~~
-
-function gtag(
-    )
-{
-    window.dataLayer.push( arguments );
+    if ( GoogleTagManagerTrackingIsEnabled )
+    {
+        gtag(
+            {
+                'event': 'pageView',
+                'pagePath': window.location.pathname,
+                'pageTitle': document.title
+            }
+            );
+    }
 }
 
 // ~~
@@ -37,9 +51,6 @@ function EnableGoogleAnalyticsTracking(
     tracking_id
     )
 {
-    var
-        script;
-
     if ( !GoogleAnalyticsTrackingIsEnabled )
     {
         GoogleAnalyticsTrackingIsEnabled = true;
@@ -48,7 +59,7 @@ function EnableGoogleAnalyticsTracking(
         GoogleAnalyticsTrackingScript.async = true;
         GoogleAnalyticsTrackingScript.src = "https://www.googletagmanager.com/gtag/js?id=" + tracking_id;
 
-        document.head.appendChild( GoogleAnalyticsTrackingScript );
+        document.head.insertBefore( GoogleAnalyticsTrackingScript, document.head.firstChild );
 
         window.dataLayer = window.dataLayer || [];
 
@@ -65,9 +76,6 @@ function DisableGoogleAnalyticsTracking(
     tracking_id
     )
 {
-    var
-        script;
-
     if ( GoogleAnalyticsTrackingIsEnabled
          && GoogleAnalyticsTrackingId === tracking_id )
     {
@@ -78,5 +86,43 @@ function DisableGoogleAnalyticsTracking(
         GoogleAnalyticsTrackingIsEnabled = false;
         GoogleAnalyticsTrackingId = "";
         GoogleAnalyticsTrackingScript = null;
+    }
+}
+
+// ~~
+
+function EnableGoogleTagManagerTracking(
+    tracking_id
+    )
+{
+    if ( !GoogleTagManagerTrackingIsEnabled )
+    {
+        GoogleTagManagerTrackingIsEnabled = true;
+        GoogleTagManagerTrackingId = tracking_id;
+        GoogleTagManagerTrackingScript = document.createElement( "script" );
+        GoogleTagManagerTrackingScript.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${tracking_id}');`;
+
+        document.head.insertBefore( GoogleTagManagerTrackingScript, document.head.firstChild );
+
+        window.dataLayer = window.dataLayer || [];
+
+        TrackRoute();
+    }
+}
+
+// ~~
+
+function DisableGoogleTagManagerTracking(
+    tracking_id
+    )
+{
+    if ( GoogleTagManagerTrackingIsEnabled
+         && GoogleTagManagerTrackingId === tracking_id )
+    {
+        document.head.removeChild( GoogleTagManagerTrackingScript );
+
+        GoogleTagManagerTrackingIsEnabled = false;
+        GoogleTagManagerTrackingId = "";
+        GoogleTagManagerTrackingScript = null;
     }
 }
